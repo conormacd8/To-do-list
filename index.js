211c26212d5f940339a1fd6b121e166fba30ce8b
Conor MacDonald <conormacd8@gmail.com>
2022-06-18 18:41:16 +1000

    Added 'delete' button functionality

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const taskContainer = document.getElementById('task-container')
 const submitTaskBtn = document.getElementById('submit-task-btn')
 const userInput = document.getElementById('user-input')
+const listItems = document.getElementsByClassName('list-item')
 
 submitTaskBtn.addEventListener('click', () => {
   if (userInput.value){
-    taskContainer.innerHTML += `<li>${userInput.value}</li>`
+    let randomId = `${Math.random()}`
+    taskContainer.innerHTML += `<li class="list-item ${randomId}">${userInput.value} <button class="delete" onclick="deleteItem(${randomId})">✅</button></li>`
     userInput.value = ''
   }
 })
+
+function deleteItem(randomId){
+  for (let i = 0 ; i < listItems.length ; i++){
+    if (listItems[i].classList.contains(randomId)){
+    listItems[i].remove(listItems[i])
+  }
+  }
+  
+}
